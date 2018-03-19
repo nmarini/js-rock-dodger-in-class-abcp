7c930c8e1d7b206b53207587335ca708b3d6e390
Nicolas Marini <nmarini44@gmail.com>
2018-03-19 23:18:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,6 +67,7 @@ function endGame() {
   ROCKS.forEach(function(rock) { rock.remove() })
 
   document.removeEventListener('keydown', moveDodger)
+  
 
   START.innerHTML = 'Play again?'
   START.style.display = 'inline'
